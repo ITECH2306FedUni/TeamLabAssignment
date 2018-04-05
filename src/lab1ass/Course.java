462d11f00769af1333153e5f5419898d7cf43287
Nathan <nathanblaney98@gmail.com>
2018-04-05 20:47:50 +1000

    Menu works plus reformat

--- src/lab1ass/Course.java
+++ src/lab1ass/Course.java
@@ -1,56 +1,58 @@
 package lab1ass;
+
 /**
  * @author RAWR xD
  */
 public class Course {
 
-	private String name;
-	private float price;
-	private int runtime;
+    private String name;
+    private float price;
+    private int runtime;
 
-	public Course(String _name, float _price , int  _runtime){
-		this.name = _name;
-		this.price = _price;
-		this.runtime = _runtime;
-		
-	}
+    public Course(String _name, float _price, int _runtime) {
+        this.name = _name;
+        this.price = _price;
+        this.runtime = _runtime;
 
-	public void finalize() throws Throwable {
+    }
 
-	}
+    public void finalize() throws Throwable {
 
-	public String getName() {
-		return name;
-	}
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public float getPrice() {
-		return price;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public void setPrice(float price) {
-		this.price = price;
-	}
+    public float getPrice() {
+        return price;
+    }
 
-	public int getRuntime() {
-		return runtime;
-	}
+    public void setPrice(float price) {
+        this.price = price;
+    }
 
-	public void setRuntime(int runtime) {
-		if (runtime > 0 && runtime <= 10){
-			this.runtime = runtime;
-		}
-	
-	}
-	public String runTimeToString(int runtime)
-	{
-		return String.valueOf(runtime);
-	}
-	@Override
-	public String toString() {
-		return "Course [course=" + name + ", price=" + price + ", runtime=" + runtime + "]";
-	}
+    public int getRuntime() {
+        return runtime;
+    }
+
+    public void setRuntime(int runtime) {
+        if (runtime > 0 && runtime <= 10) {
+            this.runtime = runtime;
+        }
+
+    }
+
+    public String runTimeToString(int runtime) {
+        return String.valueOf(runtime);
+    }
+
+    @Override
+    public String toString() {
+        return "Course [course=" + name + ", price=" + price + ", runtime=" + runtime + "]";
+    }
 }
