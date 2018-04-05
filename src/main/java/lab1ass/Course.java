8be2cfb5b0f4c772678b6a2b6b3a0600fae7552f
Nathan <nathanblaney98@gmail.com>
2018-04-05 21:41:56 +1000

    Oi we have gradle

---
+++ src/main/java/lab1ass/Course.java
@@ -0,0 +1,58 @@
+package lab1ass;
+
+/**
+ * @author RAWR xD
+ */
+public class Course {
+
+    private String name;
+    private float price;
+    private int runtime;
+
+    public Course(String _name, float _price, int _runtime) {
+        this.name = _name;
+        this.price = _price;
+        this.runtime = _runtime;
+
+    }
+
+    public void finalize() throws Throwable {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getRuntime() {
+        return runtime;
+    }
+
+    public void setRuntime(int runtime) {
+        if (runtime > 0 && runtime <= 10) {
+            this.runtime = runtime;
+        }
+
+    }
+
+    public String runTimeToString(int runtime) {
+        return String.valueOf(runtime);
+    }
+
+    @Override
+    public String toString() {
+        return "Course [course=" + name + ", price=" + price + ", runtime=" + runtime + "]";
+    }
+}
