6773ebdae56de57a3eb16611a83bad8982c839e7
Nathan <nathan@blaney.xyz>
2018-04-09 21:24:39 +1000

    Add Cat, Dog and Rabbit classes

---
+++ src/main/java/lab1ass/Rabbit.java
@@ -0,0 +1,14 @@
+package lab1ass;
+
+public class Rabbit extends Animal {
+    /**
+     * @param _breed  breed of the animal
+     * @param _name   name of the animal
+     * @param _gender gender of the animal
+     * @param _dob    dob of the animal DD MMM YYYY
+     * @param _regdue date that reg is due DD MMM YYYY
+     */
+    public Rabbit(String _breed, String _name, String _gender, String _dob, String _regdue) {
+        super(_breed, _name, _gender, _dob, _regdue);
+    }
+}
--- src/main/java/lab1ass/TestDriverClass.java
+++ src/main/java/lab1ass/TestDriverClass.java
@@ -14,20 +14,20 @@ public class TestDriverClass {
 
     public static void main(String[] args) {
         // create test case 1 - a person owning a pet dog
-        Person p1 = new Person("25 somewhere Street", "Kathleen", "2000");
-        Animal myPet = new Animal("CockerSpaniel", "Fido");
+        Person p1 = new Person("25 somewhere Street", "Kathleen", "2000", "Casey");
+        Animal myPet = new Dog("Dog", "Fido", "F","05 01 1998", "05 01 1998");
         p1.addAPet(myPet);
         System.out.println("I have a person object : " + p1.toString());
 
         // create test case 2 - another person owing a spider
-        Person p2 = new Person("12 curly avenue", "Fred", "3350");
-        Animal p2Pet = new Animal("Spider", "Leonard");
+        Person p2 = new Person("12 curly avenue", "Fred", "3350", "Casey");
+        Animal p2Pet = new Rabbit("Rabbit", "Leonard", "F","05 01 1998", "05 01 1998");
         p2.addAPet(p2Pet);
         System.out.println("I have a second person object : " + p2);
 
         // create test case 3 - another person owing a pet dog
-        Person p3 = new Person("12 curly avenue", "Mark", "3350");
-        Animal p3Pet = new Animal("Big Mother Fucking Spider", "Leonard");
+        Person p3 = new Person("12 curly avenue", "Mark", "3350", "Casey");
+        Animal p3Pet = new Cat("Cat", "Leonard", "F","05 01 1998", "05 01 1998");
         p3.addAPet(p3Pet);
         System.out.println("I have a third person object : " + p3);
     }
