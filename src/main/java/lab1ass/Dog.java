159de3ccd9506d185811357e1cb5b1e5a003b2ef
Nine-H <nine.gentooman@gmail.com>
2018-04-11 14:17:03 +1000

    added desexed property and restricted breeds data to dog class

--- src/main/java/lab1ass/Dog.java
+++ src/main/java/lab1ass/Dog.java
@@ -1,7 +1,6 @@
 package lab1ass;
 
 public class Dog extends Animal {
-
     /**
      * @param _breed  breed of the animal
      * @param _name   name of the animal
@@ -9,7 +8,18 @@ public class Dog extends Animal {
      * @param _dob    dob of the animal DD MMM YYYY
      * @param _regdue date that reg is due DD MMM YYYY
      */
-    public Dog(String _breed, String _name, String _gender, String _dob, String _regdue) {
+	
+	String[] restricted_breeds = {
+			"Dogo Argentino",
+			"Fila Brasileiro",
+			"Japanese Tosa",
+			"Perro de Presa Canario",
+			"Presa Canario"
+	};
+	boolean desexed;
+	
+    public Dog(String _breed, String _name, String _gender, String _dob, String _regdue, boolean _desexed) {
         super(_breed, _name, _gender, _dob, _regdue);
+        this.desexed = _desexed;
     }
 }
