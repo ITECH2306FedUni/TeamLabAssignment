7359c6027c862b1bb2f7b0ce43391dcb4fd909f1
Nine-H <nine.gentooman@gmail.com>
2018-05-03 11:25:49 +1000

    Initialized OnlineCouse Class

---
+++ src/main/java/lab1ass/OnlineCourse.java
@@ -0,0 +1,7 @@
+package lab1ass;
+
+public class OnlineCourse extends Course {
+	public OnlineCourse (int _studentID, String _name, float _price, int _runtime) {
+		super(_studentID, _name, _price, _runtime);
+	}
+}
