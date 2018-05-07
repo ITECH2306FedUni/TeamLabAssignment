ff81dc32af3e9eeaaaeb218308d2202e58d6158b
Nine-H <nine.gentooman@gmail.com>
2018-05-07 11:00:18 +1000

    Hi Nathan, I thought it'd be OK to go ahead and create some course subclasses:

---
+++ src/main/java/lab2ass/EveningCourse.java
@@ -0,0 +1,7 @@
+package lab2ass;
+
+public class EveningCourse extends Course {
+	public EveningCourse (int _studentID, String _name, float _price, int _runtime) {
+		super(_studentID, _name, _price, _runtime);
+	}
+}
